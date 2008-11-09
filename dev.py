4b0c015b93b48024e40d38cc00275f71224566f7
Chen Zheng <nkchenz@gmail.com>
2008-11-09 14:23:40 +0800

    dev

---
+++ dev.py
@@ -0,0 +1,36 @@
+
+from oodict import OODict
+from util import *
+
+import time
+import hashlib
+
+class Dev:
+    def __init__(self, path = None):
+        if path:
+            self.config_manager = ConfigManager(os.path.join(path))
+            self.config_file = 'config'
+            self.config = self.config_manager.load(self.config_file, OODict())
+    
+    def init(self, args):
+        # path, host, size are all in args
+        args.used = 0
+        args.status = 'offline'
+        #args.data_type = 'chunk' #chunk, meta
+        args.type = 'file'  #raid, mirror, file, log, disk
+        str = '%s %s %s' % (time.time(), args.host, args.path)
+        args.id = hashlib.sha1(str).hexdigest()
+        self.config = OODict(args)
+    
+    def assert_status(self, status):
+        if not self.config:
+            print 'not formatted'
+            sys.exit(-1)
+        
+        if self.config.status not in status:
+            print 'status not in', status
+            sys.exit(-1)
+    
+    def change_status(self, status):
+        self.config.status = status
+        self.config_manager.save(self.config, self.config_file)
