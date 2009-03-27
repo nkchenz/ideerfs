ef947f4baa0651abb5b8761d705eb0814b26d436
Chen Zheng <nkchenz@gmail.com>
2009-03-27 08:34:10 +0800

    daemonize keyword paras: stdin, stdout, stderr

--- chunk_server.py
+++ chunk_server.py
@@ -8,9 +8,9 @@ from chunk import *
 
 init_logging(os.path.join(config.home, 'chunk_server.log'))
 server = NIOServer()
+server.daemonize()
 server.set_pid_file(os.path.join(config.home, 'chunk_server.pid'))
 server.register('chunk', ChunkService())
 server.bind(config.chunk_server_address)
-server.daemonize()
 server.mainloop()
 
--- storage_server.py
+++ storage_server.py
@@ -8,9 +8,9 @@ from storage import *
 
 init_logging(os.path.join(config.home, 'storage_server.log'))
 server = NIOServer()
+server.daemonize()
 server.set_pid_file(os.path.join(config.home, 'storage_server.pid'))
 server.register('storage', StorageService())
 server.bind(config.storage_server_address)
-server.daemonize()
 server.mainloop()
 
