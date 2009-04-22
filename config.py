27c4f61f86a12e75d590a201253aae2420e4f52f
Chen Zheng <nkchenz@gmail.com>
2009-04-22 18:08:59 +0800

    easy way to config chunk server address

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 """Config file for the filesystem """
 
 import os
+import socket
 
 #--------------Please Modify these to proper values-------------
 meta_server_address = ('localhost', 1984)
 storage_server_address = ('localhost', 1985)
-chunk_server_address = ('localhost', 1986)
 # Meta device
 meta_dev = '/home/chenz/source/ideerfs/test/tmp/sd1'
 
@@ -14,11 +14,14 @@ meta_dev = '/home/chenz/source/ideerfs/test/tmp/sd1'
 # This better be a attribute of file
 checksum_algo = '' # No checksum needed
 
-#--------------No need to change these----------------------------
+daemon = False #True # Servers run as daemon
+
+
+#--------------No need to change the following----------------------------
 config_dir = '.ideerfs'
 home = os.path.join(os.getenv('HOME'), config_dir)
 if not os.path.exists(home):
     os.mkdir(home)
 
-daemon = False #True # Servers run as daemon
-
+# What should we do if chunk server has multi NICs?
+chunk_server_address = (socket.gethostname(), 1986)
