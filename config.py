b02f5ef86ae9c2c002fe730e0d6bdf8f13bc3050
Chen Zheng <zchen@novell.com>
2009-05-05 14:47:58 +0800

    change ports to 41984:41986

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ import os
 import socket
 
 #--------------Please Modify these to proper values-------------
-meta_server_address = ('localhost', 1984)
-storage_server_address = ('localhost', 1985)
+# Please modify your firewall setting to open these ports
+meta_server_address = ('localhost', 41984)
+storage_server_address = ('localhost', 41985)
+chunk_server_address = ('localhost', 41986)
 # Meta device
 meta_dev = '/home/chenz/source/ideerfs/test/tmp/sd1'
 
@@ -21,6 +23,3 @@ config_dir = '.ideerfs'
 home = os.path.join(os.getenv('HOME'), config_dir)
 if not os.path.exists(home):
     os.mkdir(home)
-
-# What should we do if chunk server has multi NICs?
-chunk_server_address = (socket.gethostname(), 1986)
