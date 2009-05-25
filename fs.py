4dc6928c127f7f98a3101c27c5c441966356f56e
Chen Zheng <nkchenz@gmail.com>
2009-05-26 00:21:45 +0800

    sync code

--- fs.py
+++ fs.py
@@ -11,6 +11,8 @@ from util import *
 import config
 from logging import info, debug
 from msg import messager
+import thread
+import threading
 
 class FileSystem:
     """Filesystem interface at client node
@@ -94,12 +96,22 @@ class FileSystem:
       
 
 class File:
+    """
+    We must provide such an interface that servral GBs of data can be written
+    to remote network in seconds.
+    """
     def __init__(self, name, fs):
         self.name = name
         self._fs = fs
         self._read_buffer = '' # Client buffer
         self._write_buffer = '' # Sequent write buffer
-        
+
+        self._write_workers = []
+        self._idle_queue = []
+        self._idle_queue_cv = threading.Condition()
+
+        self._closing = False
+
         # Next byte to read or write
         self._pos = 0
 
@@ -110,6 +122,11 @@ class File:
     def close(self):
         """Close file, flush buffers"""
         self.flush()
+        # Tell all workers to exit, some workers maybe sleep right now, so need to
+        # feed somthing to wake them up
+        self._closing = True
+        for i in self._write_workers:
+            self._feed_worker(i, 'bye') # This arbitrary string will not be written
     
     def lock(self, range):
         # Before read and write, better get a lock 
@@ -149,7 +166,7 @@ class File:
         return dids
     
     def read(self, length = None):
-        """Read len bytes from current pos
+        """Read len bytes from current pos, maybe multi chunks
 
         @length
 
@@ -209,6 +226,10 @@ class File:
     def write(self, data):
         """Buffer small writes, only write full chunks to network"""
         self._write_buffer += data
+        dl = len(data)
+        # first send out buffer and part of data
+        # then send out slices of data 
+        # until we get a non-full chunk, and put it to buffer
         csize = self.meta.chunk_size
         while True:
             offset = self._pos % csize # Usually 0, aligned by chunk
@@ -219,10 +240,70 @@ class File:
                 break
             length = csize - offset
             debug('file.write: chunk full offset %d length %d', offset, length)
-            self.write_chunk(self._write_buffer[:length])
+            self.write_chunk(self._write_buffer[:length]) # Should contains offset in file
+            self._pos += length
             self._write_buffer = self._write_buffer[length:]
 
-    def write_chunk(self, data):
+    def _sched_write(self, chunk):
+        """Sched chunk writing to a worker and return, no wait for IO """
+        if len(self._write_workers) < self.meta.pwrites:
+            i = OODict()
+            i.data = None
+            i.data_cv = threading.Condition()
+            i.id = len(self._write_workers)
+            self._write_workers.append(i)
+            #start new worker
+            thread.start_new_thread(self._write_worker, (i))
+        else:
+            # Find a idle worker whose data is None, sleep until found   
+            self._idle_queue_cv.acquire()
+            while not self._idle_queue:
+                debug('no idle worker found, wait')
+                self._idle_queue_cv.wait()
+            i = self._idle_queue.pop(0)
+            debug('idle worker %d found', i.id)
+            self._idle_queue_cv.release()
+
+        self._feed_worker(i, chunk)
+
+    def _feed_worker(self, i, data):
+        i.data_cv.acquire()
+        debug('feed worker %d', i.id)
+        i.data = data
+        i.data_cv.notify()
+        i.data_cv.release()
+
+    def _write_worker(self, i)
+        """ # Every worker should has its own queue, better scale
+        # If using global queue, thunder-herd problem?
+        """
+        debug('write worker %d started', i.id)
+        while True:
+            i.data_cv.acquire()
+            while not i.data:
+                i.data_cv.wait() # No data available, sleep
+            # Even we release the lock, it's impossible for main thread to 
+            # feed us again, because we are not on the idle queue
+            i.data_cv.release()
+
+            # Check for closing file
+            if self._closing:
+                break
+
+            debug('worker %d working', i.id)
+            self._write_data(i.data)
+            i.data = None
+
+            # Notiy we are idle, OK to feed us again
+            self._idle_queue_cv.acquire()
+            debug('worker %d idle', i.id)
+            self._idle_queue.append(i)
+            self._idle_queue_cv.notify()
+            self._idle_queue_cv.release()
+
+        debug('write worker %d closed', i.id)
+
+    def _write_data(self, data):
         """Write data to chunk, offset in chunk is self._pos % meta.chunk_size"""
         if not data:
             return 0 # Zero bytes written
@@ -258,7 +339,7 @@ class File:
         # Update size and chunk location
         # See whether we are writing out of file, update file size if yes
         # Fixme! We may have problem here if not all w_len bytes are written
-        self._pos += length
+        #self._pos += length
         if self._pos > meta.size:
             attrs.size = self._pos # Update file size
         if not new:
